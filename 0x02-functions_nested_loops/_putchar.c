793cfee90914b09e1d6065513d93f185e46ceb3b
iammoussaab <iammoussaab@gmail.com>
2023-03-26 09:47:14 -0700

    Task 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <unistd.h>
 
 /**
@@ -11,26 +10,4 @@
 int _putchar (char a)
 {
         return (write(1, &a, 1));
-}          
-=======
-#include "main.h"
-
-/**
- *main - Prints the putchar function
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
 }
->>>>>>> 564f5e1249ddeffcca7560f961b8b4543b612c35
