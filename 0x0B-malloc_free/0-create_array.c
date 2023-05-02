e2e26478533c7a9793bb265942baa187df8d9182
iammoussaab <iammoussaab@gmail.com>
2023-05-02 09:00:27 -0700

    Update Task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
+	if (str == NULL)
+		return (NULL);
 	if (size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
