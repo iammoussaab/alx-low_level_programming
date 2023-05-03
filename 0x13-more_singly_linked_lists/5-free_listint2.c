369144941e258000f8a8dd58d738d5ab563a101e
iammoussaab <iammoussaab@gmail.com>
2023-05-03 13:54:36 -0700

    Task 0-5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,18 +2,23 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * @head: head of a list.
+ *
+ * Return: no return.
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *curr;
 
-	if (head == NULL)
-		while (*head)
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
 		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
+			curr = curr->next;
+			free(temp);
 		}
-	*head = NULL;
+		*head = NULL;
+	}
 }
