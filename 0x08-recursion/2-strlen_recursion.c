de68cd911d54c3858c711229c9605129aacf7f9a
iammoussaab <iammoussaab@gmail.com>
2023-10-01 10:35:12 -0700

    style of documentation

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-    int length = 0;
-    
-    if (*s)
-    {
-        length++;
-        length += _strlen_recursion(s + 1);
-    }
-    
-    return (length);
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
 }
