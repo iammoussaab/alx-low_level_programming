b235d4687338c451eea62d0f69be11b7ffefa732
iammoussaab <iammoussaab@gmail.com>
2023-10-07 15:30:03 -0700

    ArgV and ArgC exercise

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _atoi(char *s);
+int name(int argc __attribute__((unused)), char *argv[]);
+int numb(int argc, char *argv[]);
+int numbe(int argc, char *argv[]);
+int mul(char *str);
+int check_num(int argc, char *argv[]);
+
+#endif
+
