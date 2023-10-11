b24ce5a9a3b0c23132a6fde2e68132489d40bd54
iammoussaab <iammoussaab@gmail.com>
2023-10-10 17:07:38 -0700

    Corrections of Malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
-/**		
-* count_word - helper function to count the number of words in a string		
-* @s: string to evaluate		
-* Return: number of words		
+/**
+* count_word - helper function to count the number of words in a string
+* @s: string to evaluate
+* Return: number of words
 */
-int count_word(char *s)		
+int count_word(char *s)
 {
 	int flag, c, w;
 
@@ -27,13 +27,13 @@ int count_word(char *s)
  * **strtow - splits a string into words
  * @str: string to split
  * Return: pointer to an array of strings (Success)
- * or NULL (Error)		
+ * or NULL (Error)
  */
-char **strtow(char *str)		
+char **strtow(char *str)
 {
-	char **matrix, *tmp;		
+	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-	
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
@@ -42,7 +42,7 @@ char **strtow(char *str)
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)	
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
@@ -61,7 +61,7 @@ char **strtow(char *str)
 			}
 		}
 		else if (c++ == 0)
-			start = i;	
+			start = i;
 	}
 	matrix[k] = NULL;
 	return (matrix);
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,15 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int i, ci;
 
-	if (s1 == NULL && s2 == NULL){
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	i = ci = 0;
-	while (s1[i] != '\0' && s2[ci] != '\0'){
+	while (s1[i] != '\0')
 		i++;
+	while (s2[ci] != '\0')
 		ci++;
-	}
 	cat = malloc(sizeof(char) * (i + ci + 1));
 	if (cat == NULL)
 		return (NULL);
