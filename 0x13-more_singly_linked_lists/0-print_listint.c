75628187ff9f4dd22ad61b8dcefe69644c45effc
iammoussaab <iammoussaab@gmail.com>
2023-10-24 02:09:05 -0700

    errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a list.
@@ -18,4 +19,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (nod);
 }
-
